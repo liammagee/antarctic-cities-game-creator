dcfc1d000c602e5b936c84331a75b23f4b5c3a51
liammagee <liam.magee@gmail.com>
2020-06-19 01:17:59 +1000

    Add back mask

--- build/web-mobile/main.8caa7.js
+++ build/web-mobile/main.8caa7.js
@@ -179,7 +179,7 @@ window.boot = function () {
 if (window.jsb) {
     var isRuntime = (typeof loadRuntime === 'function');
     if (isRuntime) {
-        require('src/settings.a5295.js');
+        require('src/settings.00ff9.js');
         require('src/cocos2d-runtime.js');
         if (CC_PHYSICS_BUILTIN || CC_PHYSICS_CANNON) {
             require('src/physics.js');
@@ -187,7 +187,7 @@ if (window.jsb) {
         require('jsb-adapter/engine/index.js');
     }
     else {
-        require('src/settings.a5295.js');
+        require('src/settings.00ff9.js');
         require('src/cocos2d-jsb.js');
         if (CC_PHYSICS_BUILTIN || CC_PHYSICS_CANNON) {
             require('src/physics.js');
