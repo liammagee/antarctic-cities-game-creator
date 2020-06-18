82ed4ce039d1e089dc00bac93e82adaffa28f93d
liammagee <liam.magee@gmail.com>
2020-06-19 01:37:12 +1000

    Add high res images to build

--- build/web-mobile/main.28032.js
+++ build/web-mobile/main.28032.js
@@ -179,7 +179,7 @@ window.boot = function () {
 if (window.jsb) {
     var isRuntime = (typeof loadRuntime === 'function');
     if (isRuntime) {
-        require('src/settings.20a97.js');
+        require('src/settings.0746b.js');
         require('src/cocos2d-runtime.js');
         if (CC_PHYSICS_BUILTIN || CC_PHYSICS_CANNON) {
             require('src/physics.js');
@@ -187,7 +187,7 @@ if (window.jsb) {
         require('jsb-adapter/engine/index.js');
     }
     else {
-        require('src/settings.20a97.js');
+        require('src/settings.0746b.js');
         require('src/cocos2d-jsb.js');
         if (CC_PHYSICS_BUILTIN || CC_PHYSICS_CANNON) {
             require('src/physics.js');
