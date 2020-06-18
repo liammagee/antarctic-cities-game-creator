5426262cfb79bb954bf1e3549c775ae6e01a3365
liammagee <liam.magee@gmail.com>
2020-06-18 17:04:03 +1000

    Add mask

--- build/web-mobile/main.27874.js
+++ build/web-mobile/main.27874.js
@@ -141,7 +141,7 @@ window.boot = function () {
     // jsList
     var jsList = settings.jsList;
 
-    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.6834c.js';
+    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.b3f0e.js';
     if (jsList) {
         jsList = jsList.map(function (x) {
             return 'src/' + x;
@@ -179,7 +179,7 @@ window.boot = function () {
 if (window.jsb) {
     var isRuntime = (typeof loadRuntime === 'function');
     if (isRuntime) {
-        require('src/settings.b8251.js');
+        require('src/settings.29831.js');
         require('src/cocos2d-runtime.js');
         if (CC_PHYSICS_BUILTIN || CC_PHYSICS_CANNON) {
             require('src/physics.js');
@@ -187,7 +187,7 @@ if (window.jsb) {
         require('jsb-adapter/engine/index.js');
     }
     else {
-        require('src/settings.b8251.js');
+        require('src/settings.29831.js');
         require('src/cocos2d-jsb.js');
         if (CC_PHYSICS_BUILTIN || CC_PHYSICS_CANNON) {
             require('src/physics.js');
